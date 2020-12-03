ca21e7183893be5bc4f0c65b1de924f67aba6684
Valerie <53797814+valerievazq@users.noreply.github.com>
2020-12-02 20:18:02 -0700

    api 1 project done

---
+++ database.js
@@ -0,0 +1,45 @@
+let users = [
+  { id: "1", name: "Samantha Jones", bio: "Fancy Marketing Agent" },
+  { id: "2", name: "Carrie Bradshaw", bio: "Vogue Columnist" },
+  { id: "3", name: "Jack Doe", bio: "Lawyer" },
+];
+
+function getUsers() {
+  return users;
+}
+
+function getUserById(id) {
+  return users.find((u) => u.id === id);
+}
+
+function createUser(data) {
+  const payload = {
+    id: String(users.length + 1),
+    ...data,
+  };
+
+  users.push(payload);
+  return payload;
+}
+
+function updateUser(id, data) {
+  const index = users.findIndex((u) => u.id === id);
+  users[index] = {
+    ...users[index],
+    ...data,
+  };
+
+  return users[index];
+}
+
+function deleteUser(id) {
+  users = users.filter((u) => u.id != id);
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
